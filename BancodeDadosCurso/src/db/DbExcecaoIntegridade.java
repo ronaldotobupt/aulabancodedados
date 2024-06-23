fc7ce4c2000048a210a4e683abd0d65e90ccf757
Ronaldo Santos <ronaldo.tboupt@gmail.com>
2024-06-23 17:48:23 -0300

    CRUD - DELETE(APAGANDO DADOS) - Apagando dados do banco

---
+++ BancodeDadosCurso/src/db/DbExcecaoIntegridade.java
@@ -0,0 +1,11 @@
+package db;
+
+public class DbExcecaoIntegridade extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public DbExcecaoIntegridade (String msg) {
+		super (msg);
+	}
+
+}
